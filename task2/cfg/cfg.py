1b8d0c6ec674b8e21d1cc4c9369766984c11596b
arw274 <arw274@cornell.edu>
2025-09-08 16:58:06 -0400

    added tests

---
+++ task2/cfg/cfg.py
@@ -0,0 +1,44 @@
+import sys
+import json
+
+def basic_blocks(instrs):
+    blocks = [[]]
+    labels = {}
+    for inst in instrs:
+        if "label" in inst and len(blocks[-1]) > 0:
+            blocks.append([])
+        blocks[-1].append(inst)
+        if "label" not in inst and inst["op"] in ("br, jmp"):
+            blocks.append([])
+    for i, b in enumerate(blocks): 
+        if len(b) == 0:
+            print("Warning: empty block detected")
+        elif "label" in b[0]:
+            print("Block {}: {}".format(i, b))
+            labels[b[0]["label"]] = i
+        else:
+            print("Block {}: {}".format(i, b))
+    return [b for b in blocks if len(b) > 0], labels
+
+def cfg(blocks, labels):
+    graph = {}
+    for i, b in enumerate(blocks):
+        if "op" in b[-1] and b[-1]["op"] in ("br", "jmp"):
+            graph[i] = [labels[l] for l in b[-1]["labels"]]
+        elif len(blocks) > i+1:
+            graph[i] = [i+1]
+        else:
+            graph[i] = []
+    return graph
+
+def all_cfgs(full_bril):
+    ret = {}
+    for f in full_bril["functions"]:
+        if len(full_bril) > 1: print("Function:", f["name"])
+        ret[f["name"]] = cfg(*basic_blocks(f["instrs"]))
+    return ret
+
+if __name__ == "__main__":
+    full_bril = json.load(sys.stdin)
+    cfgs = all_cfgs(full_bril)
+    print(cfgs)
--- task2/simple_analysis/simple_analysis.py
+++ task2/simple_analysis/simple_analysis.py
@@ -1,35 +1,5 @@
-def basic_blocks(instrs):
-    blocks = [[]]
-    for inst in instrs:
-        if "label" in inst and len(blocks[-1]) > 0:
-            blocks.append([])
-        blocks[-1].append(inst)
-        if "label" not in inst and inst["op"] in ("br, jmp"):
-            blocks.append([])
-    return [b for b in blocks if len(b) > 0]
-
-def block_name(block):
-    if "label" in block[0]:
-        return block[0]["label"]
-    else:
-        return "_entry"
-
-def cfg(blocks):
-    graph = {}
-    for i, b in enumerate(blocks):
-        if "op" in b[-1] and b[-1]["op"] in ("br", "jmp"):
-            graph[block_name(b)] = b[-1]["labels"]
-        elif len(blocks) > i+1:
-            graph[block_name(b)] = [block_name(blocks[i+1])]
-        else:
-            graph[block_name(b)] = []
-    return graph
-
-def all_cfgs(full_bril):
-    ret = {}
-    for f in full_bril["functions"]:
-        ret[f["name"]] = cfg(basic_blocks(f["instrs"]))
-    return ret
+import sys
+import json
 
 def call_graph(full_bril):
     ret = {}
@@ -39,3 +9,8 @@ def call_graph(full_bril):
             if "op" in i and i["op"] == "call":
                 ret[f["name"]].update(i["funcs"])
     return ret
+
+if __name__ == "__main__":
+    full_bril = json.load(sys.stdin)
+    cg = call_graph(full_bril)
+    print(cg)
