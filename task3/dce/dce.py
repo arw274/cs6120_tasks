9d27715cc37fe8342d0a24211236c960394ccca0
Amanda Wang <amandawang@Amandas-MacBook-Pro.local>
2025-09-14 10:47:55 -0400

    dce

---
+++ task3/dce/dce.py
@@ -0,0 +1,55 @@
+import sys, os
+import json
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
+from task2.cfg.cfg import basic_blocks 
+
+def globally_unused_vars(func):
+    """Remove all variables that are defined but never used."""
+    used = set()
+    for instr in func["instrs"]:
+        if "args" in instr and instr["args"]:
+            used.update(instr["args"])
+    
+    filtered_instrs = []
+    for instr in func["instrs"]:
+        if "dest" in instr and instr["dest"] not in used:
+            continue
+        filtered_instrs.append(instr)
+    func["instrs"] = filtered_instrs
+
+def iterate(func):
+    """Iteratively apply optimizations until no changes occur."""
+    while True:
+        before = len(func["instrs"])
+        globally_unused_vars(func)
+        after = len(func["instrs"])
+        if before == after:
+            break
+
+def locally_killed_instrs(func):
+    """Remove instructions that are overwritten before they are used within the same block."""
+    blocks, _ = basic_blocks(func["instrs"], quiet=True)
+    for block in blocks:
+        used = {}
+        for instr in reversed(block):
+            if "args" in instr and instr["args"]:
+                for arg in instr["args"]:
+                    used[arg] = True
+            if "dest" in instr and instr["dest"] in used:
+                if used[instr["dest"]] == False:
+                    block.remove(instr)
+                else:
+                    used[instr["dest"]] = False
+    func["instrs"] = [instr for block in blocks for instr in block]
+    
+def dce(full_bril):
+    for func in full_bril["functions"]:
+        iterate(func)
+        locally_killed_instrs(func)
+    return full_bril
+
+if __name__ == "__main__":
+    full_bril = json.load(sys.stdin)
+    optimized_bril = dce(full_bril)
+    print(json.dumps(optimized_bril, indent=2))
