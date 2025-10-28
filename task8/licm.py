db6d7b6ddbe1665cc99a8fb5867653a168fd5c05
natetyoung <natetyoung42@gmail.com>
2025-10-28 14:10:39 -0400

    real licm with actual transformation

--- task8/licm.py
+++ task8/licm.py
@@ -7,7 +7,7 @@ from task4.worklist import flip_cfg
 from task5.dominators import dominators
 from task8.natural_loops import natural_loops
 
-SIDE_EFFECT_OPS = ("call", "div")
+SIDE_EFFECT_OPS = ("call", "div", "set")
 
 def create_preheaders(func):
     blocks, labels = basic_blocks(func["instrs"], quiet=True)
@@ -24,7 +24,7 @@ def create_preheaders(func):
         for i in reverse_graph[h]:
             if i not in l:
                 outside_loop_preds.append(i)
-        if len(outside_loop_preds) > 1:
+        if len(outside_loop_preds) > 1 or len(graph[outside_loop_preds[0]]) > 1:
             # create preheader
             label = blocks[h][0]["label"]
             blocks[h].insert(0, {"label": label+"__preheader"})
@@ -35,7 +35,7 @@ def create_preheaders(func):
                     blocks[b][-1]["labels"][idx] = label+"__preheader"
     func["instrs"] = [instr for block in blocks for instr in block]
 
-def single_loop_licm(all_blocks, cfg, doms, loop, preheader): # DOES NOT WORK ON SSA
+def single_loop_licm(all_blocks, cfg, doms, loop, preheader_idx):
     loop = list(loop)
     # add everything defined in loop to non-LI list
     li_instrs = []
@@ -49,24 +49,7 @@ def single_loop_licm(all_blocks, cfg, doms, loop, preheader): # DOES NOT WORK ON
                     var_num_defs[instr["dest"]] += 1
                 else:
                     var_num_defs[instr["dest"]] = 1
-    # until convergence:
-    changed = True
-    while changed:
-        # add all unique-dest instrs with no non-LI args to LI instr list and remove from non_li
-        changed = False
-        for block in loop:
-            for j, instr in enumerate(all_blocks[block]):
-                if (block, j) not in li_instrs and "dest" in instr and var_num_defs[instr["dest"]] == 1:
-                    if "args" not in instr or all([a not in non_li_vars for a in instr["args"]]):
-                        li_instrs.append((block,j))
-                        non_li_vars.remove(instr["dest"])
-                        changed = True
-    print(li_instrs)
-    # move all to preheader if:
-        # instr dominates exits OR BOTH
-        # instr has no side effects AND
-        # value is dead after loop
-    to_move = []
+    
     exits = []
     for block in loop:
         if any([c not in loop for c in cfg[block]]):
@@ -77,13 +60,33 @@ def single_loop_licm(all_blocks, cfg, doms, loop, preheader): # DOES NOT WORK ON
             for instr in block:
                 if "args" in instr:
                     used_outside_loop.update(instr["args"])
-    for i, j in li_instrs:
-        if all([i in doms[e] for e in exits]):
-            to_move.append((i, j))
-        elif blocks[i][j]["op"] not in SIDE_EFFECT_OPS:
-            if "dest" not in blocks[i][j] or blocks[i][j]["dest"] not in used_outside_loop:
-                            to_move.append((i, j))
-    print(to_move)
+    # until convergence:
+    changed = True
+    while changed:
+        # add all unique-dest instrs with no non-LI args to LI instr list and remove from non_li
+        changed = False
+        for block in loop:
+            to_move_now = []
+            for j, instr in enumerate(all_blocks[block]):
+                if (block, j) not in li_instrs and "dest" in instr and var_num_defs[instr["dest"]] == 1:
+                    if "args" not in instr or all([a not in non_li_vars for a in instr["args"]]):
+                        # loop invariant instr
+                        move = False
+                        if all([block in doms[e] for e in exits]):
+                            move = True
+                        elif instr["op"] not in SIDE_EFFECT_OPS:
+                            if "dest" not in blocks[block][j] or blocks[block][j]["dest"] not in used_outside_loop:
+                                move = True
+                        if move:
+                            li_instrs.append((block,j))
+                            non_li_vars.remove(instr["dest"])
+                            to_move_now.append(j)
+                            changed = True
+            ph_end = len(all_blocks[preheader_idx])
+            for j in to_move_now[::-1]:
+                # delete jth element of block
+                # add it to the end of preheader
+                all_blocks[preheader_idx].insert(ph_end, all_blocks[block].pop(j))
 
 if __name__ == "__main__":
     full_bril = json.load(sys.stdin)
@@ -92,10 +95,12 @@ if __name__ == "__main__":
         blocks, labels = basic_blocks(func["instrs"], quiet=True)
         entry = 0  # Assuming the first block is the entry block
         graph = reachable_cfg(cfg(blocks, labels), entry)
-        print(graph)
+        # print(graph)
         reverse_graph = flip_cfg(graph)
         doms = dominators(graph, entry)
-        print(labels)
+        # print(labels)
         for l in natural_loops(graph, doms):
-            print(l)
-            single_loop_licm(blocks, graph, doms, l[1], None)
+            # print(l)
+            single_loop_licm(blocks, graph, doms, l[1], reverse_graph[l[0]][0])
+        func["instrs"] = [instr for block in blocks for instr in block]
+    print(json.dumps(full_bril))
